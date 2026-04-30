b3a74b9fa79cc9fef25e8f99a13a36c69e73df95
prasiddha <prasiddhadahal33@gmail.com>
2026-04-30 17:59:25 +0545

    song controller done

---
+++ backend/src/controllers/song.controller.js
@@ -0,0 +1,35 @@
+const imageKit = require('../config/imagekit')
+const songModel = require('../models/song.model')
+
+const uploadSong = async(req,res) => {
+    try{
+        
+        const {title} = req.body;
+
+        const file = req.file.audio[0];   // this comes form multer
+
+        const audioResponse = await imagekit.upload({    //uploads it to imagekit cloud storage and it returns url
+            file: file.buffer.toString("base64"),
+            fileName: file.originalname
+        })
+
+        const song = await songModel.create({
+
+            title,
+            artist: req.user.id,
+            audioUrl : audioResponse.url
+        });
+
+        res.status(201).json({
+            message:"song uploaded successfully",
+            song
+        })
+
+    }catch(error){
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+module.exports = uploadSong
